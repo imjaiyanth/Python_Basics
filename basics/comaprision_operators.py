2c1c652519d6d57df686df697abac3540c06ca4a
Suhitha R <imjaiyanth@gmail.com>
2024-07-27 22:19:39 +0530

    pushing the learnings

---
+++ basics/comaprision_operators.py
@@ -0,0 +1,19 @@
+bmw = 56.78
+audii = 85.34
+# checks if its equal or not
+print(bmw==audii)
+
+# checks if its not eqaul or not
+print(bmw!=audii)
+
+# checks if bmw is greater than audii
+print(bmw>audii)
+
+# checks if bmw is smaller than audii
+print(bmw<audii)
+
+# checks if bmw is greater than or equal to audii
+print(bmw>=audii)
+
+# checks if bmw is smaller than or equal to audii
+print(bmw<=audii)
---
+++ basics/even0to50.py
@@ -0,0 +1,8 @@
+# for x in range(50):
+#     if x%2!=0 :
+#      print(x)
+
+
+for x in range(1,100,2):
+  print(x)
+
